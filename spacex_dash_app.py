7aa3239555a33dd29bd07bcccccaa1a981cdabb1
Crisel Nublo 🪻 <167146347+CriselPy@users.noreply.github.com>
2024-12-08 00:05:29 +0000

    up

--- spacex_dash_app.py
+++ spacex_dash_app.py
@@ -318,4 +318,5 @@ def update_map(selected_site):
 
 # Correr el servidor 
 if __name__ == "__main__":
-    app.run_server(debug=False)  # Render se encarga del servidor en producción
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(host='0.0.0.0', port=port, debug=False)  # Render se encarga del servidor en producción
